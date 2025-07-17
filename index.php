0eb3557a917b6f35bdfb34035d67eda0be4e4dfe
KiratiNa004 <664230004@webmail.npru.ac.th>
2025-07-17 12:54:07 +0700

    17/7/68

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>>Welcome to php basic</h1>
+    <p>This is a simple PHP application</p>
+    <hr>
+    <h1 style="color:red">MY MENU</h1>
+
+    <ul>
+        <li> <a href="W01_01_introPHP.php">Intro PHP</a></li>
+        <li> <a href="W02_01_oddEvenNumber.php">Odd-Even-number</a></li>
+        <li> <a href="W02_02_grade.php">Calculater Grade</a></li>
+        <li> <a href="calculate_monney.php">Calculater Grade 01</a></li>
+    </ul>
+
+
+
+</body>
+
+</html>
